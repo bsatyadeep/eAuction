e3535ba419fdd5bade7aee8161b1bbf0929f80ff
satyadeep_b <sbehera@uei.com>
2016-12-06 16:36:43 +0530

    Add project files.

---
+++ eAuction/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eAuction.MvcApplication" Language="C#" %>
---
+++ eAuction/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eAuction.Startup))]
+namespace eAuction
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
