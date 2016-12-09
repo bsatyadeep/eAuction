d32a6ea1c787219aeee926fd910e95ec50b82233
satyadeep_b <sbehera@uei.com>
2016-12-09 18:48:38 +0530

    updated new changes

---
+++ eAuction/Controllers/AuctionsController.cs
@@ -0,0 +1,51 @@
+﻿using eAuction.Models;
+using System;
+using System.Web.Mvc;
+
+namespace eAuction.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        // GET: Autions
+        public ActionResult Index()
+        {
+            var auctions = new[] {
+                new Auction
+                {
+                    Title = "Example Auction #1",
+                    Description = "This is a demo Auction #1",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddDays(7),
+                    StartPrice = 1.00m,
+                    CurrentPrice = null
+                },
+                new Auction
+                {
+                    Title = "Example Auction #2",
+                    Description = "This is a demo Auction #2",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddDays(7),
+                    StartPrice = 1.00m,
+                    CurrentPrice = null
+
+                }
+            };
+            return View(auctions);
+        }
+
+        public ActionResult Auction()
+        {
+            var auction = new Auction
+            {
+                Title = "Example Auction #1",
+                Description = "This is a demo Auction #1",
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddDays(7),
+                StartPrice = 1.00m,
+                CurrentPrice = null
+            };
+            //ViewData["Auction"] = auction;
+            return View(auction);
+        }
+    }
+}
---
+++ eAuction/Models/Auction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace eAuction.Models
+{
+    public class Auction
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImgageUrl { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public decimal StartPrice { get; set; }
+        public decimal? CurrentPrice { get; set; }
+    }
+}
